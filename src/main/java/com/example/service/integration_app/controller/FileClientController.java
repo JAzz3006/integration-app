54ad879458c35788ca3c0e2aa67bf00621b95129
Grigoriy Chernykh <che90@mail.ru>
2026-07-03 15:41:05 +0500

    in progress-2

---
+++ src/main/java/com/example/service/integration_app/controller/FileClientController.java
@@ -0,0 +1,37 @@
+package com.example.service.integration_app.controller;
+import com.example.service.integration_app.clients.OkHttpClientSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/client/file")
+public class FileClientController {
+
+    private final OkHttpClientSender client;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestPart MultipartFile file){
+//        client.uploadFile(file);
+//        return ResponseEntity.ok("File uploaded successfully"); - вариант спикера
+        String response = client.uploadFile(file);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName){
+        Resource resource = client.downloadFile(fileName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
+    }
+
+}
