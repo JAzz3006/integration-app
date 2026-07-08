18827fee1651ff7f7c87be3bc8393e68761eff58
Grigoriy Chernykh <che90@mail.ru>
2026-07-08 16:11:06 +0500

    in progress-4

--- src/main/java/com/example/service/integration_app/clients/RestTemplateClient.java
+++ src/main/java/com/example/service/integration_app/clients/RestTemplateClient.java
@@ -1,6 +1,11 @@
 package com.example.service.integration_app.clients;
+import com.example.service.integration_app.model.EntityModel;
+import com.example.service.integration_app.model.UpsertEntityRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -8,14 +13,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class RestTemplateClient {
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     @Value("${app.integration.base-url}")
     private String baseUrl;
@@ -40,6 +47,38 @@ public class RestTemplateClient {
                  fileName
         );
         return response.getBody();
+    }
+
+    public List<EntityModel> getEntityList(){
+        ResponseEntity<List<EntityModel>> response = restTemplate.exchange(
+                baseUrl + "/api/v1/entity",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                new ParameterizedTypeReference<List<EntityModel>>() {}
+        );
+        return response.getBody();
+    }
+
+    public EntityModel getEntityModelByName(String name){
+        ResponseEntity<EntityModel> response = restTemplate.exchange(
+                baseUrl + "/api/v1/entity/{name}",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                EntityModel.class,
+                name
+        );
+        return response.getBody();
+    }
+
+    public EntityModel createEntityModel(UpsertEntityRequest request){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return restTemplate.exchange(
+                baseUrl + "/api/v1/entity",
+                HttpMethod.POST,
+                new HttpEntity<>(request, headers),
+                EntityModel.class
+        ).getBody();
     }
 
 }
