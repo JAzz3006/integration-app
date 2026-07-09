2aaae4fa865120e4f22cd6df55321aa92c94fccc
Grigoriy Chernykh <che90@mail.ru>
2026-07-09 23:30:12 +0500

    in progress still-1

--- src/main/java/com/example/service/integration_app/clients/RestTemplateClient.java
+++ src/main/java/com/example/service/integration_app/clients/RestTemplateClient.java
@@ -13,7 +13,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- src/main/java/com/example/service/integration_app/clients/WebClientSender.java
+++ src/main/java/com/example/service/integration_app/clients/WebClientSender.java
@@ -1,12 +1,21 @@
 package com.example.service.integration_app.clients;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
@@ -20,11 +29,32 @@ public class WebClientSender {
                 .filename(file.getOriginalFilename())
                 .contentType(MediaType.APPLICATION_OCTET_STREAM);
         webClient.post()
-                .uri("/api/v1/client/file/upload")
+                .uri("/api/v1/file/upload")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
                 .body(BodyInserters.fromMultipartData(builder.build()))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
+    }
+
+    @SneakyThrows
+    public void downloadFile(String fileName){
+        Resource resource = webClient.get()
+                .uri("/api/v1/file/{fileName}", fileName)
+                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .retrieve()
+                .bodyToMono(Resource.class)
+                .block();
+        if (resource == null){
+            throw new IllegalStateException("Response is null");
+        }
+        Path dir = Paths.get("download");
+        Files.createDirectories(dir);
+        Path filePath = dir.resolve(fileName);
+        Files.copy(
+               resource.getInputStream(),
+               filePath,
+               StandardCopyOption.REPLACE_EXISTING
+        );
     }
 }
